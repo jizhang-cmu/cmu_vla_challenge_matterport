3e9ca838ceda0ee2b221429bfc31c3abd8ae9d2a
Ji Zhang <zhangji@cmu.edu>
2023-10-02 12:58:36 -0400

    minor update

--- system_bring_up.sh
+++ system_bring_up.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+CONDA_DIR="/home/$USER/anaconda3"
+CONDA_BIN_DIR="$CONDA_DIR/bin"
+CONDA_SETUP_FILE="$CONDA_DIR/bin/conda"
+CONDA_PROFILE_FILE="$CONDA_DIR/etc/profile.d/conda.sh"
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/$USER/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$($CONDA_SETUP_FILE 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/home/$USER/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/$USER/anaconda3/etc/profile.d/conda.sh"
+    if [ -f $CONDA_PROFILE_FILE ]; then
+        . $CONDA_PROFILE_FILE
     else
-        export PATH="/home/$USER/anaconda3/bin:$PATH"
+        export PATH="$CONDA_BIN_DIR:$PATH"
     fi
 fi
 unset __conda_setup
 # <<< conda initialize <<<
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 conda activate habitat
 cd $SCRIPT_DIR/src/segmentation_proc/scripts
